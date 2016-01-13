4127e86e31e6f0db273ffc9fb2ba65500782746d
Emmanuel Morales <emmanuel.morales@headspring.com>
2016-01-13 15:09:59 -0600

    Additional fix for deployment

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,51 +2,51 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'source-map'
 
-  entry: {
-    app: './demo/src/main.js',
-    vendor: ['react', 'react-dom', 'react-bootstrap', 'react-stack']
-  },
+  ,entry: {
+    app: './demo/src/main.js'
+    ,vendor: ['react', 'react-dom', 'react-bootstrap']
+  }
 
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js',
-    publicPath: '/public/'
-  },
+  ,output: {
+    path: path.join(__dirname, 'public')
+    ,filename: '[name].js'
+    ,publicPath: '/public/'
+  }
 
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
+  ,plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+    ,new webpack.optimize.DedupePlugin()
+    ,new webpack.optimize.UglifyJsPlugin({
+      minimize: true
+      ,compress: {
         warnings: false
       }
     })
-  ],
+  ]
 
-  resolve: {
-    root: path.resolve(__dirname),
-    extensions: ['', '.js', '.jsx'],
-    alias: {
+  ,resolve: {
+    root: path.resolve(__dirname)
+    ,extensions: ['', '.js', '.jsx']
+    ,alias: {
       'react-stack': 'dist/react-stack'
     }
-  },
+  }
 
-  module: {
+  ,module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel', include: path.join(__dirname, 'demo/src') },
-      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
-      { test: /\.scss?$/, loader: 'style!css!sass', include: path.join(__dirname, 'demo/src') },
+      { test: /\.jsx?$/, loader: 'babel', include: path.join(__dirname, 'demo/src') }
+      ,{ test: /\.js?$/, loader: 'babel', exclude: /node_modules/ }
+      ,{ test: /\.scss?$/, loader: 'style!css!sass', include: path.join(__dirname, 'demo/src') }
 
       // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
       // loads bootstrap's css.
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
+      ,{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" }
+      ,{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" }
+      ,{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" }
+      ,{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" }
+      ,{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   }
 }
